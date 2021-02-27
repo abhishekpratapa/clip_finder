08366fe88b2af31055a513d6f26d100090753db1
Abhishek Pratapa <abhishekpratapa@gmail.com>
2021-02-27 04:13:11 -0600

    Added 4 cameras

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,47 @@
 using namespace cv;
 
 int main(int argc, char* argv[]) {
-  cv::VideoCapture camera(0);
-  if (!camera.isOpened()) {
-    std::cerr << "ERROR: Could not open camera" << std::endl;
+  cv::VideoCapture camBottomRight(0);
+  cv::VideoCapture camTopRight(1);
+  cv::VideoCapture camBottomLeft(3);
+  cv::VideoCapture camTopLeft(4);
+
+  if (!camBottomRight.isOpened() || !camTopRight.isOpened() || !camBottomLeft.isOpened() || !camTopLeft.isOpened()) {
+    std::cerr << "ERROR: Could not open cameras" << std::endl;
     return 1;
   }
   // create a window to display the images from the webcam
   cv::namedWindow("Webcam", WINDOW_AUTOSIZE);
 
   // this will contain the image from the webcam
+  cv::Mat camBRFrame;
+  cv::Mat camTRFrame;
+  cv::Mat camBLFrame;
+  cv::Mat camTLFrame;
+
+  cv::Mat camRightFrame;
+  cv::Mat camLeftFrame;
+
   cv::Mat frame;
 
-  // capture the next frame from the webcam
-  camera >> frame;
-
-  // display the frame until you press a key
   while (1) {
+    camBottomRight >> camBRFrame;
+    camTopRight >> camTRFrame;
+    camBottomLeft >> camBLFrame;
+    camTopLeft >> camTLFrame;
+
+    cv::vconcat(camTRFrame, camBRFrame, camRightFrame);
+    cv::vconcat(camTLFrame, camBLFrame, camLeftFrame);
+    cv::hconcat(camRightFrame, camLeftFrame, frame);
+
     // show the image on the window
     cv::imshow("Webcam", frame);
     // wait (10ms) for a key to be pressed
     if (cv::waitKey(10) >= 0) break;
   }
-  camera.release();
+  camBottomRight.release();
+  camTopRight.release();
+  camBottomLeft.release();
+  camTopLeft.release();
   return 0;
 }
