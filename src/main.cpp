4b4289e4c384fc90a39dff2f17c4f65a855feb61
Abhishek Pratapa <abhishekpratapa@gmail.com>
2021-02-27 03:54:09 -0600

    Updated WindowSize Flag

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
@@ -11,37 +10,22 @@ int main(int argc, char* argv[]) {
     std::cerr << "ERROR: Could not open camera" << std::endl;
     return 1;
   }
+  // create a window to display the images from the webcam
+  cv::namedWindow("Webcam", WINDOW_AUTOSIZE);
+
+  // this will contain the image from the webcam
+  cv::Mat frame;
+
+  // capture the next frame from the webcam
+  camera >> frame;
+
+  // display the frame until you press a key
+  while (1) {
+    // show the image on the window
+    cv::imshow("Webcam", frame);
+    // wait (10ms) for a key to be pressed
+    if (cv::waitKey(10) >= 0) break;
+  }
   camera.release();
   return 0;
 }
-
-// #include "opencv2/opencv.hpp"
-// #include "iostream"
-
-// int main(int, char**) {
-//     // open the first webcam plugged in the computer
-//     cv::VideoCapture camera(0);
-//     if (!camera.isOpened()) {
-//         std::cerr << "ERROR: Could not open camera" << std::endl;
-//         return 1;
-//     }
-
-//     // create a window to display the images from the webcam
-//     cv::namedWindow("Webcam", CV_WINDOW_AUTOSIZE);
-
-//     // this will contain the image from the webcam
-//     cv::Mat frame;
-        
-//     // capture the next frame from the webcam
-//     camera >> frame;
-    
-//     // display the frame until you press a key
-//     while (1) {
-//         // show the image on the window
-//         cv::imshow("Webcam", frame);
-//         // wait (10ms) for a key to be pressed
-//         if (cv::waitKey(10) >= 0)
-//             break;
-//     }
-//     return 0;
-// }
