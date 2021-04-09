ea6eb68bccbc1e951997a56604e4e5750d3f3d3d
Abhishek <abhishekpratapa@gmail.com>
2021-04-08 19:36:27 -0500

    Cleaned up darknet

--- src/detector.cpp
+++ src/detector.cpp
@@ -21,6 +21,7 @@ namespace {
 
 constexpr size_t kInputWidth = 416;
 constexpr size_t kInputHeight = 416;
+constexpr float kPixelScale = 1.0 / 255.0;
 constexpr bool kSwapRBChannels = true;
 constexpr bool kCrop = false;
 
@@ -29,11 +30,10 @@ const cv::Size kInputSize = {kInputWidth, kInputHeight};
 std::vector<Clip> PostProcessing(cv::Mat& frame, std::vector<cv::Mat>& outputs,
                                  float confidence_threshold) {
   std::vector<Clip> clips;
-
-  for (size_t i = 0; i < outputs.size(); ++i) {
-    float* data = reinterpret_cast<float*>(outputs[i].data);
-    for (int j = 0; j < outputs[i].rows; ++j, data += outputs[i].cols) {
-      cv::Mat scores = outputs[i].row(j).colRange(5, outputs[i].cols);
+  for (auto& output : outputs) {
+    float* data = reinterpret_cast<float*>(output.data);
+    for (int j = 0; j < output.rows; ++j, data += output.cols) {
+      cv::Mat scores = output.row(j).colRange(5, output.cols);
       cv::Point classIdPoint;
       double confidence;
       cv::minMaxLoc(scores, 0, &confidence, 0, &classIdPoint);
@@ -51,7 +51,6 @@ std::vector<Clip> PostProcessing(cv::Mat& frame, std::vector<cv::Mat>& outputs,
       }
     }
   }
-
   return clips;
 }
 
@@ -68,20 +67,22 @@ Detector::Detector(Config& config)
 Detector::~Detector() = default;
 
 void Detector::CheckOutputType() {
-  std::vector<int> out_layers = model_.getUnconnectedOutLayers();
-
-  for (auto& out : out_layers) {
-    std::string out_layer_type = model_.getLayer(out)->type;
-    assert(out_layer_type == "Region");
-  }
+  static std::vector<int> out_layers = model_.getUnconnectedOutLayers();
+  for (auto& out : out_layers) assert(model_.getLayer(out)->type == "Region");
 }
 
 std::vector<Clip> Detector::Predict(cv::Mat& frame) {
   static cv::Mat blob;
   std::vector<cv::Mat> outputs;
 
-  cv::dnn::blobFromImage(frame, blob, (1.0 / 255.0), kInputSize, cv::Scalar(),
-                         kSwapRBChannels, kCrop, CV_32F);
+  cv::dnn::blobFromImage(frame,           /* Frame from [0 - 255] */
+                         blob,            /* Normalized output */
+                         kPixelScale,     /* Pixel scale factor */
+                         kInputSize,      /* Network input size */
+                         cv::Scalar(),    /* Subtract nothing from the pixels */
+                         kSwapRBChannels, /* Swap the R and B channels */
+                         kCrop,           /* Don't crop the image */
+                         CV_32F);         /* 32 Bit Color Channel */
 
   model_.setInput(blob);
   model_.forward(outputs, out_layer_names_);
