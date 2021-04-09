e33a0572d02871ff323d85087a0242c19aab8a5c
Abhishek Pratapa <abhishekpratapa@gmail.com>
2021-04-08 20:57:55 -0500

    Added Multiple Cameras

--- app/main.cpp
+++ app/main.cpp
@@ -28,37 +28,82 @@ constexpr char kWeightsPath[] =
 }  // namespace
 
 int main(int argc, char* argv[]) {
-  cv::VideoCapture camBottomRight(1);
+    cv::VideoCapture camBottomRight(0);
+  cv::VideoCapture camTopRight(1);
+  cv::VideoCapture camBottomLeft(3);
+  cv::VideoCapture camTopLeft(4);
 
-  if (!camBottomRight.isOpened()) {
+  if (!camBottomRight.isOpened() || !camTopRight.isOpened() ||
+      !camBottomLeft.isOpened() || !camTopLeft.isOpened()) {
     std::cerr << "ERROR: Could not open cameras" << std::endl;
     return 1;
   }
-
+  // create a window to display the images from the webcam
   cv::namedWindow("Webcam", cv::WINDOW_AUTOSIZE);
-  cv::Mat camBRFrame;
 
-  clip_finder::Config config(kConfigPath, kWeightsPath, {"clip"}, 0.75, 0.5);
-  clip_finder::processing::Detector detector(config);
+  // this will contain the image from the webcam
+  cv::Mat camBRFrame;
+  cv::Mat camTRFrame;
+  cv::Mat camBLFrame;
+  cv::Mat camTLFrame;
+
+  cv::Mat camRightFrame;
+  cv::Mat camLeftFrame;
+
+  cv::Mat frame;
 
   while (1) {
-    try {
-      camBottomRight >> camBRFrame;
-      auto clips = detector.Predict(camBRFrame);
-      for (auto& c : clips) {
-        cv::Rect rect(c.pos_x - c.width / 2, c.pos_y - c.height / 2, c.width,
-                      c.height);
-        cv::rectangle(camBRFrame, rect, cv::Scalar(0, 255, 0));
-      }
-      // show the image on the window
-      cv::imshow("Webcam", camBRFrame);
-    } catch (...) {
-    }
+    camBottomRight >> camBRFrame;
+    camTopRight >> camTRFrame;
+    camBottomLeft >> camBLFrame;
+    camTopLeft >> camTLFrame;
 
+    cv::vconcat(camTRFrame, camBRFrame, camRightFrame);
+    cv::vconcat(camTLFrame, camBLFrame, camLeftFrame);
+    cv::hconcat(camRightFrame, camLeftFrame, frame);
+
+    // show the image on the window
+    cv::imshow("Webcam", frame);
     // wait (10ms) for a key to be pressed
     if (cv::waitKey(10) >= 0) break;
   }
-
   camBottomRight.release();
+  camTopRight.release();
+  camBottomLeft.release();
+  camTopLeft.release();
+  return 0;
+
+  // cv::VideoCapture camBottomRight(1);
+
+  // if (!camBottomRight.isOpened()) {
+  //   std::cerr << "ERROR: Could not open cameras" << std::endl;
+  //   return 1;
+  // }
+
+  // cv::namedWindow("Webcam", cv::WINDOW_AUTOSIZE);
+  // cv::Mat camBRFrame;
+
+  // clip_finder::Config config(kConfigPath, kWeightsPath, {"clip"}, 0.75, 0.5);
+  // clip_finder::processing::Detector detector(config);
+
+  // while (1) {
+  //   try {
+  //     camBottomRight >> camBRFrame;
+  //     auto clips = detector.Predict(camBRFrame);
+  //     for (auto& c : clips) {
+  //       cv::Rect rect(c.pos_x - c.width / 2, c.pos_y - c.height / 2, c.width,
+  //                     c.height);
+  //       cv::rectangle(camBRFrame, rect, cv::Scalar(0, 255, 0));
+  //     }
+  //     // show the image on the window
+  //     cv::imshow("Webcam", camBRFrame);
+  //   } catch (...) {
+  //   }
+
+  //   // wait (10ms) for a key to be pressed
+  //   if (cv::waitKey(10) >= 0) break;
+  // }
+
+  // camBottomRight.release();
   return 0;
 }
